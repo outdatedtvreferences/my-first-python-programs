7d4ddd7bb7b0e3cfc5b634e082c5b1c0e61d1847
outdatedtvreferences <31477735+outdatedtvreferences@users.noreply.github.com>
2017-08-31 09:27:46 -0400

    Add files via upload

---
+++ my-first-python-programs/hello.py
@@ -0,0 +1,18 @@
+# This program says hello and greets a person by name.
+#
+# David
+# August 24, 2017 
+
+print("hello.")
+print("what is your name?")
+David = input()
+print("It is good to meet you," + David + ".")
+print("where were you born, David?")
+Greenville = input()
+print("have you ever been to another state?")
+yes = input()
+print("where?")
+Virginia = input()
+print("cool")
+
+
