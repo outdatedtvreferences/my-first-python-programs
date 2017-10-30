910f8ae4dbe65e5aaa5136cbe3dd6aaf6bd12cff
outdatedtvreferences <31477735+outdatedtvreferences@users.noreply.github.com>
2017-10-30 09:35:12 -0400

    Add files via upload

---
+++ my-first-python-programs/math_demo.py
@@ -0,0 +1,16 @@
+# This program demonstrates how to use the math module.
+#
+# David Burton
+# August 30, 2017
+
+import math
+
+# the math module has constants
+print(math.pi)
+print(math.e)
+
+#the math module also has built in functions
+
+print(math.sqrt(2))
+print(math.sin(0.5))
+
